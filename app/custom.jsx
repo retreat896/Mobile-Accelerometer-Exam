0bba3d6062a805348be24ebfc5331b35b74a5e97
Jacob Malland <mallandj@uwplatt.edu>
2025-10-11 18:04:52 -0500

    Added Balloon and Dart rendering (beta)   Added:   - Balloon SVG   - String SVG   - Dart SVG

--- app/custom.jsx
+++ app/custom.jsx
@@ -1,6 +1,6 @@
 import { useRef, useCallback } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { Dimensions } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import { GLView } from 'expo-gl';
 import { Renderer, THREE } from 'expo-three';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
@@ -8,8 +8,123 @@ import { useFocusEffect } from '@react-navigation/native';
 import getMainStyles from '../styles/main';
 import useAccelerometer from '../modules/accelerometer';
 import Navigation from '../components/navigation';
+import { File, Paths } from 'expo-file-system';
+import { Asset } from 'expo-asset';
+
+
+// The Balloon SVG files
+const BalloonSVG = Asset.fromModule(require('../assets/Balloon.svg'));
+const StringSVG = Asset.fromModule(require('../assets/Balloon_String.svg'));
+const DartSVG = Asset.fromModule(require('../assets/Dart.svg'));
 
 global.THREE = global.THREE || THREE;
+
+// Helper to load SVG file contents from a relative path
+/**
+ * Get SVG file contents from an Asset
+ * @param {Asset} asset 
+ */
+async function GetSVG(asset) {
+    // The Asset hasn't been downloaded yet
+    if (!asset.downloaded) await asset.downloadAsync();
+
+    // After downloading, return the file contents
+    const file = new File({ uri: asset.localUri });
+    const svgContent = await file.text();
+
+    return svgContent;
+};
+
+// The static SVG assets
+let Balloon, String, Dart = '';
+
+// Load the SVG file contents
+(async () => {
+    Balloon = await GetSVG(BalloonSVG);
+    String = await GetSVG(StringSVG);
+    Dart = await GetSVG(DartSVG);
+})();
+
+/**
+ * Function to render an SVG for the scene
+ * @returns A SVG Object for rendering
+ */
+function RenderSVG(svgData, color, opacity) {
+    if (!svgData) throw new Error("Missing SVG data");
+
+    // Load SVG
+    const Loader = new SVGLoader();
+    const Paths = Loader.parse(svgData).paths;
+
+    // Create the shapes for the Object, based on the SVG data
+    const Shapes = [];
+    Paths.forEach(path => {
+      const pathShapes = path.toShapes(true);
+      Shapes.push(...pathShapes);
+    });
+
+    // Create the Object's geometry
+    const Geometry = new THREE.ShapeGeometry(Shapes[0]);
+    Geometry.center();
+
+    // The material to use
+    const Material = new THREE.MeshBasicMaterial({
+      color: color, // 0x000000, // black
+      side: THREE.DoubleSide,
+      transparent: true,
+      opacity: opacity // 1,
+    });
+
+    // Return the Object assembled from the material and geometry data
+    return(new THREE.Mesh(Geometry, Material));
+}
+
+/**
+ * Create a Balloon to display
+ * @returns Balloon 3D Object
+ */
+function CreateBalloon() {
+    console.log(Balloon);
+
+    // Get the SVG object
+    const balloonSVG = RenderSVG(Balloon, 0xe02100, 1);
+
+    balloonSVG.scale.set(0.002, -0.001, 0.002); // Scale appropriately
+    balloonSVG.rotateY(90); // Rotate 90 degrees
+
+    // Get the String SVG object
+    const stringSVG = RenderSVG(String, 0x00000, 1);
+
+    // Keep same scale as Balloon
+    stringSVG.scale.set(0.002, -0.001, 0.002);
+
+    // Format String position to be in-line with the balloon's end.
+    // This is an approximation
+    stringSVG.position.y -= 0.6;
+    stringSVG.position.x += 0.095;
+
+    // Make the Balloon a single group, to render as one
+    const balloon = new THREE.Group();
+    balloon.add(balloonSVG);
+    balloon.add(stringSVG);
+
+    return balloon;
+}
+
+/**
+ * Create a dart to display
+ * @returns Dart 3D Object
+ */
+function CreateDart() {
+
+    // Get the SVG object
+    const dartSVG = RenderSVG(Dart, 0x444444, 1);
+
+    // Set the proper scale for the dart
+    dartSVG.scale.set(0.005, -0.005, 0.005);
+
+    return dartSVG;
+}
 
 const useThreeScene = (aRef) => {
     const physicsRef = useRef({
@@ -20,6 +135,7 @@ const useThreeScene = (aRef) => {
     });
 
     const dartRef = useRef(null);
+    const balloonRef = useRef(null);
     const animationFrameIdRef = useRef(null);
     const threeRefs = useRef({ renderer: null, scene: null, camera: null });
     const { width, height } = Dimensions.get('window');
@@ -36,39 +152,19 @@ const useThreeScene = (aRef) => {
         );
         camera.position.z = 5;
 
-        // SVG path data for the dart
-        const svgData = `
-        <svg xmlns="http://www.w3.org/2000/svg">
-            <path d="M136.221,27.244c2.638-11.053-2.09-24.443-2.09-24.443s-10.62,4.008-13.2,7.827C120.282,8.1,118.6,2.5,118.6,2.5s-1.687,5.6-2.326,8.128C113.68,6.809,103.059,2.8,103.059,2.8s-5.4,13.572-2.09,24.443c1.2,3.947,8.523,12.351,13.345,20.451,1.73,20.64-.585,27.683-2.127,32.421v21.313c0,4.535,3.892,7.814,4.243,8.231l1.987,25.788.042-.535.041.535,1.987-25.788c.356-.417,4.673-3.7,4.673-8.231V80.467c0-.119-.005-.232-.009-.351h.009c-1.532-4.709-3.824-11.708-2.155-32.053,4.842-7.868,12.083-16.07,13.216-20.819Z" />
-        </svg>`;
-
-        const loader = new SVGLoader();
-        const svgPaths = loader.parse(svgData).paths;
-        
-        // Create shape from SVG path
-        const shapes = [];
-        svgPaths.forEach(path => {
-            const pathShapes = path.toShapes(true);
-            shapes.push(...pathShapes);
-        });
-
-        const geometry = new THREE.ShapeGeometry(shapes[0]);
-        geometry.center(); // Center the geometry
-
-        const material = new THREE.MeshBasicMaterial({
-            color: 0x444444, // Dark gray color
-            side: THREE.DoubleSide,
-            transparent: true,
-            opacity: 0.9
-        });
-        
-        const dart = new THREE.Mesh(geometry, material);
+        const dart = CreateDart();
         
         // Scale and rotate the dart appropriately
         dart.scale.set(0.005, -0.005, 0.005); // Scale down the SVG and flip vertically
         
         scene.add(dart);
         dartRef.current = dart;
+
+        let balloon = CreateBalloon();
+        balloon.position.set(0, 0, 0); // Position to the right of the dart
+        
+        scene.add(balloon);
+        balloonRef.current = balloon;
 
         threeRefs.current = { renderer, scene, camera };
 
@@ -82,10 +178,16 @@ const useThreeScene = (aRef) => {
             const viewHeight = 2 * camera.position.z * Math.tan(vFOV / 2);
             const aspectRatio = width / height;
             const viewWidth = viewHeight * aspectRatio;
-            const boundX = (viewWidth / 2) - 1;
-            const boundY = (viewHeight / 2) - 1;
+            const boundX = (viewWidth / 2) - 0.5;
+            const boundY = (viewHeight / 2) - 0.5;
 
             if (renderer && scene && camera && dart) {
+                const balloon = balloonRef.current;
+                if (balloon) {
+                    // Make the balloon gently float up and down
+                    balloon.position.y = Math.sin(Date.now() * 0.001) * 0.1;
+                }
+
                 // Update velocity based on accelerometer
                 p.velocityX += -a.x * p.ACCEL_SENSITIVITY;
                 p.velocityY += -a.y * p.ACCEL_SENSITIVITY;
